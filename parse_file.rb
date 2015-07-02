21daa3f41836274b0211f0b49490edc0fb75dfde
Egon Hilgenstieler <egon@acras.com.br>
2015-07-02 11:48:02 -0300

    anotação de lembranças de campos q dao problema

--- parse_file.rb
+++ parse_file.rb
@@ -1,6 +1,6 @@
 #coding: utf-8
 
-require './parser.rb' 
+require './parser.rb'
 
 unless ARGV.count == 1
   puts "Usage: parse_file.rb [FILENAME]"
@@ -21,3 +21,4 @@ contents = f.read
 h = p.parse(contents)
 
 puts h.inspect
+puts "Verifique manualmente: cont_geral_oper_nao_fiscais, aliq_trib, tot_trib, tot_isencao_icms (I1)"
